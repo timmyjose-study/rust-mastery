83905aaaec1db954b8365a74c6e4e5cbc0fd5d1b
Timmy Jose <zoltan.jose@gmail.com>
2022-04-20 17:09:44 +0530

    [ferrous-systems-workshop] - finished the basic section

--- ferrous-systems-workshop/src/basics.rs
+++ ferrous-systems-workshop/src/basics.rs
@@ -1,7 +1,8 @@
+#![allow(dead_code)]
+
 //! Rust Basics
 
 mod basic_tyoes {
-
     #[cfg(test)]
     mod tests {
         use std::any::Any;
@@ -93,8 +94,8 @@ mod error_handling {
     }
 
     fn convert_errors() -> Result<(), Box<dyn Error>> {
-        let f = File::open("src/basic.rs")?;
-        let x = "123abc".parse::<i32>()?;
+        let _f = File::open("src/basic.rs")?;
+        let _x = "123abc".parse::<i32>()?;
 
         Ok(())
     }
@@ -137,8 +138,8 @@ mod error_handling {
 mod ownership {
     #[derive(Debug, Copy, Clone)]
     struct Dot {
-        x: i32,
-        y: i32,
+        pub x: i32,
+        pub y: i32,
     }
 
     fn pacman(dot: Dot) {
@@ -222,8 +223,264 @@ mod borrowing {
 }
 
 mod strings {
+    fn accept_either<S: AsRef<str> + ?Sized>(thing: &S) -> String {
+        String::from("foo") + thing.as_ref()
+    }
+
     #[cfg(test)]
     mod tests {
         use super::*;
+
+        #[test]
+        fn test_string_deref() {
+            let part_one = String::from("Hello ");
+            let part_two = String::from("world ");
+            let whole = part_one + &part_two + "again";
+            assert_eq!(whole, "Hello world again");
+        }
+
+        #[test]
+        fn test_split_string() {
+            let text = "Cow says moo";
+            let words = text.split(" ").collect::<Vec<_>>();
+            assert_eq!(words, ["Cow", "says", "moo"]);
+        }
+
+        #[test]
+        fn test_string_concatenation() {
+            let animal = String::from("Cow");
+            let sound = String::from("moo");
+            let text = [&animal, " says ", &sound].concat();
+            assert_eq!(text, "Cow says moo");
+        }
+
+        #[test]
+        fn test_string_replacement() {
+            let text = "Cow says moo";
+            let replaced = text.replace("moo", "woof");
+            assert_eq!(replaced, "Cow says woof");
+        }
+
+        #[test]
+        fn test_accept_either_string_or_str() {
+            assert_eq!(accept_either("foo"), "foofoo");
+            assert_eq!(accept_either(&String::from("bar")), "foobar");
+        }
+    }
+}
+
+mod iterators {
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test_owned_iterators() {
+            let v = vec![1, 2, 3, 4, 5];
+
+            for e in v.into_iter() {
+                // this consumes the vector
+                print!("{} ", e);
+            }
+            println!();
+
+            //println!("{:?}", v);
+        }
+
+        #[test]
+        fn test_borrowed_iterators() {
+            let v = vec![1, 2, 3, 4, 5];
+            for e in v.iter() {
+                // this borrows the vector
+                print!("{} ", e);
+            }
+            println!();
+
+            // so this now fine
+            println!("{:?}", v);
+        }
+
+        #[test]
+        fn test_mutably_borrowed_iterators() {
+            let mut v = vec![1, 2, 3, 4, 5];
+            for e in v.iter_mut() {
+                *e += 100;
+            }
+            println!("{:?}", v);
+        }
+
+        #[test]
+        fn test_manual_iterator() {
+            let v = vec!['a', 'b', 'c', 'd', 'e'];
+            let mut iter = v.iter();
+            while let Some(e) = iter.next() {
+                print!("{} ", e);
+            }
+            println!();
+        }
+
+        #[test]
+        fn test_combinator_map() {
+            let fizzbuzz = (0..100).map(|x| match x {
+                x if x % 15 == 0 => String::from("FizzBuzz"),
+                x if x % 3 == 0 => String::from("Fizz"),
+                x if x % 5 == 0 => String::from("Buzz"),
+                _ => format!("{}", x),
+            });
+
+            for item in fizzbuzz {
+                print!("{} ", item);
+            }
+            println!();
+        }
+
+        #[test]
+        fn test_combinator_filter() {
+            let evens = (1..100).filter(|&e| e % 2 == 0);
+
+            for e in evens {
+                print!("{} ", e);
+            }
+            println!();
+        }
+
+        #[test]
+        fn test_combinator_filter_map() {
+            // note that iterators are lazy by default
+            let evens_squared = (1..100).filter(|&x| x % 2 == 0).map(|x| x * x);
+            for e in evens_squared {
+                print!("{} ", e);
+            }
+            println!();
+        }
+    }
+}
+
+mod functions {
+    use std::fmt::Debug;
+
+    #[derive(Debug)]
+    struct Person {
+        pub name: String,
+        pub age: u16,
+    }
+
+    fn prints_anything<T: Debug>(thing: T) {
+        println!("{:?}", thing);
+    }
+
+    // this is the same as above
+    fn prints_anything_as_well<T>(thing: T)
+    where
+        T: Debug,
+    {
+        println!("{:?}", thing);
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_prints_anything() {
+            prints_anything("Hello");
+            prints_anything(100);
+            prints_anything(Person {
+                name: "Bob".to_string(),
+                age: 42,
+            });
+        }
+    }
+
+    #[test]
+    fn test_prints_anything_as_well() {
+        prints_anything_as_well("Hello");
+        prints_anything_as_well(100);
+        prints_anything_as_well(Person {
+            name: "Bob".to_string(),
+            age: 42,
+        });
+    }
+}
+
+mod stack_and_heap {
+    #[cfg(test)]
+    mod tests {
+        #[test]
+        fn test_stack_allocation() {
+            #[derive(Debug, PartialEq)]
+            struct Point {
+                x: i32,
+                y: i32,
+            }
+
+            // this is allocated on the stack
+            let point = Point { x: 1, y: -32 };
+            assert_eq!(point, Point { x: 1, y: -32 });
+        }
+
+        #[test]
+        fn test_heap_allocation() {
+            #[derive(Debug, PartialEq)]
+            struct Point {
+                x: i32,
+                y: i32,
+            }
+
+            let point = Box::new(Point { x: 1, y: -32 });
+            assert_eq!(*point, Point { x: 1, y: -32 });
+        }
+    }
+
+    #[test]
+    fn test_borrowing_box_values() {
+        #[derive(Debug)]
+        struct Person {
+            name: String,
+            age: u16,
+        }
+
+        let boxed_bob = Box::new(Person {
+            name: "Bob".to_string(),
+            age: 42,
+        });
+
+        fn print_person(p: &Person) {
+            println!("Name = {:?}, age = {:?}", p.name, p.age);
+        }
+
+        print_person(&boxed_bob);
+    }
+}
+
+mod generics {
+    #[derive(Debug, PartialEq)]
+    struct Point<Precision> {
+        x: Precision,
+        y: Precision,
+    }
+
+    #[derive(Debug, PartialEq)]
+    enum Either<L, R> {
+        Left(L),
+        Right(R),
+    }
+
+    #[cfg(test)]
+    mod tests {
+        use super::*;
+
+        #[test]
+        fn test_precision() {
+            let point = Point { x: 1, y: -32 };
+            let another_point: Point<i32> = Point { x: 1, y: -32 };
+            assert_eq!(point, another_point);
+        }
+
+        #[test]
+        fn test_either() {
+            let mut val = Either::Left::<i32, f64>(100);
+            println!("val  = {:?}", val);
+            val = Either::Right::<i32, f64>(1.2345);
+            println!("val  = {:?}", val);
+        }
     }
 }
